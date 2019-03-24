1c28dd9c2b2e758d666d8ed74adf299f905f2b4e
YairTawil <yairtawil@gmail.com>
2019-03-24 14:18:18 +0200

    refactor: types prototype check (#6)
    * refactor: types prototype check
    
    * remove comments
    
    * bump version

--- src/auto-subscriptions.ts
+++ src/auto-subscriptions.ts
@@ -1,13 +1,4 @@
-export type AutoSubscriptionsInput = string | ((...args) => any);
-export type AutoSubscriptionsDataInput = [string, ((...args) => any) | undefined];
-
 export type InputKeys = 'init' | 'destroy';
-export type AutoSubscriptionsMetaData = { [K in InputKeys]: AutoSubscriptionsInput; }
-
-export enum Errors {
-    invalidType = '"{{key}}" input must be of type "string" or "function"',
-    invalidFunction = 'could not found {{key}} function on prototype'
-}
 
 interface Subscription {
     unsubscribe();
@@ -24,24 +15,6 @@ const isObservable = (observable: Observable)  => typeof observable.subscribe ==
 export interface AutoSubscriptionProps {
     readonly _subscriptionsPropertyKeys_: string[];
     _subscriptionsList_: Subscription[];
-}
-
-function _getDataViaInput(prototype: any, key: InputKeys, input: AutoSubscriptionsInput): AutoSubscriptionsDataInput | never {
-    switch (typeof input) {
-        case 'string': {
-            return [<string> input, prototype[<string> input]];
-        }
-        case 'function':
-            const result = Object.entries<(...args) => any>(prototype).find(([key, value]: [string, any]) => value === input);
-            if (result) {
-                return result
-            } else {
-                throw new Error(Errors.invalidFunction.replace('{{key}}', key));
-            }
-
-        default:
-            throw new Error(Errors.invalidType.replace('{{key}}', key));
-    }
 }
 
 export const initSubscriptions = (instance: AutoSubscriptionProps): void => {
@@ -84,12 +57,20 @@ const AutoAutoSubscriptionsDestory = (originalDestroy) => {
     };
 };
 
-export function AutoSubscriptions({ init, destroy }: AutoSubscriptionsMetaData): any | never {
-    return function (constructor) {
-        const [initKey, originalInit] = _getDataViaInput(constructor.prototype, 'init', init);
-        const [destroyKey, originalDestroy] = _getDataViaInput(constructor.prototype, 'destroy', destroy);
-        constructor.prototype[initKey] = AutoAutoSubscriptionsInit(originalInit);
-        constructor.prototype[destroyKey] = AutoAutoSubscriptionsDestory(originalDestroy)
+export function AutoSubscriptions <VALUE extends string>(metadata: { [KEY in InputKeys]: VALUE }) {
+    return function
+        <PROTO extends { new(...args): { [K in VALUE]: (...args) => any} }>
+        (constructor: PROTO): void | never {
+        const originalInit = constructor.prototype[metadata.init];
+        const originalDestroy = constructor.prototype[metadata.destroy];
+        if (!originalInit) {
+            throw new Error(`Can\'t find init function with: ${metadata.init}`);
+        }
+        if (!originalDestroy) {
+            throw new Error(`Can't find destroy function with ${metadata.destroy}`);
+        }
+        constructor.prototype[metadata.init] = AutoAutoSubscriptionsInit(originalInit);
+        constructor.prototype[metadata.destroy] = AutoAutoSubscriptionsDestory(originalDestroy)
     };
 }
 
